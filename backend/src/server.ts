b6ca619dfd4b0575cfb263255d23100679997ee1
Dhananjaya Swain <dhananjayaswain99@gmail.com>
2025-08-15 22:38:40 +0530

    configured prisma postgresql connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,8 +3,10 @@ import cors from "cors";
 import { PORT } from "./config/env.js";
 import router from "./routes/index.js";
 import { errorHandler } from "./middleware/errorHandler.js";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
+const prisma = new PrismaClient();
 
 app.use(cors());
 app.use(express.json());
@@ -12,6 +14,19 @@ app.use(express.json());
 app.use("/api",router);
 
 app.use(errorHandler);
+
+app.get("/users", async (req, res) => {
+    const users = await prisma.user.findMany();
+    res.json(users);
+});
+
+app.post("/users", async (req, res) => {
+    const {email, name} = req.body;
+    const user = await prisma.user.create({
+        data : {email,name}
+    });
+    res.json(user);
+});
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
