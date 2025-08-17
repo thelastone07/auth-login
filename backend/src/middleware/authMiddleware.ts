7b552aeadb601d376c4e81ef6e2cbdd40c1402c2
Dhananjaya Swain <dhananjayaswain99@gmail.com>
2025-08-18 01:29:54 +0530

    added auth middleware for heavy auth

--- backend/src/middleware/authMiddleware.ts
+++ backend/src/middleware/authMiddleware.ts
@@ -1,8 +1,16 @@
 import type {Request, Response, NextFunction} from "express";
 import jwt from "jsonwebtoken";
+import { PrismaClient } from "@prisma/client";
+import type {JwtPayload as BaseJwtPayload} from "jsonwebtoken";
 
-interface JwtPayload {
-    userId : String;
+const prisma = new PrismaClient();
+
+export interface AuthRequest extends Request {
+    user ?: {id : string};
+}
+
+interface CustomJwtPayload extends BaseJwtPayload{
+    userId : string;
 }
 
 export function authMiddleware(req : Request, res : Response, next : NextFunction) {
@@ -18,14 +26,59 @@ export function authMiddleware(req : Request, res : Response, next : NextFunctio
         }
 
         const secret = process.env.JWT_SECRET!;
-        const decoded = jwt.verify(token, secret) as JwtPayload;
-
-        (req as any).user = {id : decoded.userId};
-
-
-        next();
+        const decoded = jwt.verify(token, secret);
+        
+        if (typeof decoded === 'object' && decoded && 'userId' in decoded) {
+            const payload = decoded as CustomJwtPayload;
+            (req as AuthRequest).user = {id : payload.userId};
+            next();
+        }
+        else {
+            return res.status(401).json({error : "Invalid token payload"});
+        }
+        
     }
     catch (err) {
+        return res.status(403).json({error : "Invalid or expired token"});
+    }
+}
+
+export async function requireAuth(req : AuthRequest, res : Response, next : NextFunction) {
+    try {
+
+        const authHeader = req.headers["authorization"];
+        if (!authHeader || !authHeader?.startsWith("Bearer ")) {
+            return res.status(401).json({error : "Authorization header missing or invalid"});
+        }
+
+        const token = authHeader.split(" ")[1];
+
+        if (!token) {
+            return res.status(401).json({error : "Token not found"});
+        }
+        const secret = process.env.JWT_SECRET!;
+        const decoded = jwt.verify(token, secret);
+
+        if (typeof decoded === 'object' && decoded && 'userId' in decoded) {
+            const payload = decoded as CustomJwtPayload;
+
+            const session = await prisma.session.findFirst({
+                where : {token : token},
+                include : {user : true},
+            });
+
+            if (!session || session.expiresAt < new Date()) {
+                return res.status(401).json({error : "invalid session or session expired"});
+            } 
+            req.user = {id : payload.userId};
+            next();
+        }
+        else {
+            return res.status(401).json({error : "invalid token payload"});
+        }
+
+    } catch (err) {
+        console.error("Auth error", err);
         return res.status(403).json({error : "Invalid or expired token"});
     }
 }
