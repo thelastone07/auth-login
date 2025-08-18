c1cfd4db389f79c7c770393f03df5e3e886c6629
Dhananjaya Swain <dhananjayaswain99@gmail.com>
2025-08-18 21:44:03 +0530

    deleted redudant files

--- backend/src/controllers/exampleController.ts
+++
@@ -1,5 +0,0 @@
-import type {Request, Response} from "express";
-
-export function getHello(req : Request, res : Response) {
-    res.send("Hello from Express + TypeScript");
-}
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,6 @@
 import express from "express";
 import cors from "cors";
 import { PORT } from "./config/env.js";
-import router from "./routes/index.js";
-import { errorHandler } from "./middleware/errorHandler.js";
 import { PrismaClient } from "@prisma/client";
 import { authMiddleware } from "./middleware/authMiddleware.js";
 import authRoutes from "./routes/authRoutes.js"
@@ -13,10 +11,6 @@ const prisma = new PrismaClient();
 
 app.use(cors());
 app.use(express.json());
-
-app.use("/api",router);
-
-app.use(errorHandler);
 
 app.get("/protected", authMiddleware, (req,res)=> {
     res.json({message : "This route is protected", user : (req as any).user})
