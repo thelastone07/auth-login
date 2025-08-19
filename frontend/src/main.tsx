d273cea4c0c1c7878009d39a33151531b23a9fdc
Dhananjaya Swain <dhananjayaswain99@gmail.com>
2025-08-20 02:36:49 +0530

    removed console.log(client id) lol

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { GoogleOAuthProvider } from '@react-oauth/google'
-console.log(import.meta.env.VITE_GOOGLE_CLIENT_ID);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
